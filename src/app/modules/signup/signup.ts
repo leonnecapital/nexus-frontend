0e8d2aad81e9b889845acfd6151c5969d3897583
leonecbruno <leonecbruno@gmail.com>
2025-10-28 19:15:34 +0000

    Adicionando modulos

--- src/app/modules/signup/signup.ts
+++ src/app/modules/signup/signup.ts
@@ -1,10 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ɵInternalFormsSharedModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ɵInternalFormsSharedModule],
   templateUrl: './signup.html',
   styleUrl: './signup.css',
 })
@@ -13,15 +13,15 @@ export class Signup {
   router = inject(Router);
   private formBuilder = inject(FormBuilder);
 
-  formSignUp: FormGroup = this.formBuilder.group({
+  form: FormGroup = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
     fullName: ['', [Validators.required, Validators.minLength(6)]],
     password: ['', [Validators.required, Validators.minLength(6)]]
   });
 
   doSignUp() {
-    console.log(this.formSignUp.value);
-    if (!this.formSignUp.valid) {
+    console.log(this.form.value);
+    if (!this.form.valid) {
       return;
     }
 
